f3d462fdc7de16245cce9d142fd9f21636269c39
Xi Wu <xxw289@case.edu>
2017-02-27 20:36:55 -0500

    params not working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  http_basic_authenticate_with :name => "#{ENV['SITE_NAME']}", :password => "#{ENV['SITE_PASSWORD']}"
+  http_basic_authenticate_with :name => "Xi", :password => "database"
 end
